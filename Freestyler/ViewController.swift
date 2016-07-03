27b3fa40fdba36f74d1ecdba8f96b0712aaabe00
Alexander Doloz <adoloz@cayugasoft.com>
2016-07-03 21:12:31 +0300

    Add example of styling

--- Freestyler/ViewController.swift
+++ Freestyler/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var one: UIButton!
+    @IBOutlet weak var two: UIButton!
+    @IBOutlet weak var three: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let roundCorners = Style<UIView> {
+            $0.layer.cornerRadius = 5.0
+        }
+        let background = Style<UIView> {
+            $0.backgroundColor = .yellowColor()
+        }
+        
+        let tint = Style<UIView> {
+            $0.tintColor = .redColor()
+        }
+        
+        let totalStyle = roundCorners + background + tint
+        let totalButtonStyle: Style<UIButton> = totalStyle.cast()
+        
+        [one, two, three].forEach {
+            $0 <~ totalButtonStyle
+        }
+        
+        let greenTint: Style<UIButton> = Style<UIView> {
+            $0.tintColor = .greenColor()
+        }.cast()
+        
+        two <~ greenTint
     }
 
     override func didReceiveMemoryWarning() {
