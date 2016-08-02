f8bdbd90d1941837c37d0c01f6ead6ebe9342990
Alexander Doloz <adoloz@cayugasoft.com>
2016-08-02 22:49:51 +0300

    Add UIProgressView styles

---
+++ Freestyler/Source/UIProgressViewStyles.swift
@@ -0,0 +1,26 @@
+//
+//  UIProgressViewStyles.swift
+//  Freestyler
+//
+//  Created by Alexander on 02.08.16.
+//  Copyright © 2016 Alexander. All rights reserved.
+//
+
+import UIKit
+
+
+public extension ColorType {
+    public var progressTint: Style {
+        return Style("Progress Tint \(self)") {
+            let progressView: UIProgressView = try typeChecker($0)
+            progressView.progressTintColor = self.color
+        }
+    }
+    
+    public var trackTint: Style {
+        return Style("Track Tint \(self)") {
+            let progressView: UIProgressView = try typeChecker($0)
+            progressView.trackTintColor = self.color
+        }
+    }
+}
---
+++ FreestylerTests/UIProgressViewTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+@testable import Freestyler
+
+
+class UIProgressViewTests: XCTestCase {
+    let progressView = UIProgressView()
+    
+    func testProgressViewStyles() {
+        progressView <~ DefaultPalette.A.progressTint + DefaultPalette.B.trackTint
+        XCTAssertEqual(progressView.progressTintColor, DefaultPalette.A.color)
+        XCTAssertEqual(progressView.trackTintColor, DefaultPalette.B.color)
+    }
+}
